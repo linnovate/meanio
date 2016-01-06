e323e2ef973cc3d1c73781c0c127c1f3b7ab12c8
Christopher Lowenthal <christopher.lowenthal@autodesk.com>
2016-01-06 00:01:52 -0500

    Removed mean version check. Wouldn't work even if the versions were found.

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-var pkg = require(process.cwd() + '/package.json');
-var meanVersion = pkg.mean || pkg.version;
+'use strict';
 
-if (meanVersion < '0.3.2') {
-  if (meanVersion === '0.3.1')
-    module.exports = require('./lib/mean0.3.1');
-  else
-    module.exports = require('./lib/mean0.3.0');
-} else {
-  // current version
-  module.exports = require('./lib/mean');
-}
+module.exports = require('./lib/mean');
