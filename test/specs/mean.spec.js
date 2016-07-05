e14e2da3fff766636672e496c9e3db62bdc162ce
Christopher Lowenthal <clowenthal@hybridgames.net>
2016-07-05 03:03:07 -0400

    Marks loadConfig as deprecated. loadConfig produces an error message about deprecation. Tests added for such.

--- test/specs/mean.spec.js
+++ test/specs/mean.spec.js
@@ -2,13 +2,40 @@
 
 var chai = require('chai'),
   expect = chai.expect;
+require('mocha-sinon');
 
 var mean = require('../..');
 
-describe('MEAN IO Core', function() {
-  describe('Config', function() {
-    it('should load config based on NODE_ENV and NODE_CONFIG_DIR environmental vars', function() {
+describe('MEAN IO Core', function () {
+  it('should extend Container', function() {
+    var Container = require('lazy-dependable').Container;
+    expect(mean instanceof Container).to.be.true;
+  });
+
+  describe('Config', function () {
+    it('should load config based on NODE_ENV and NODE_CONFIG_DIR environmental vars', function () {
       expect(mean.getConfig()._test).to.be.true;
     });
+  });
+
+  describe('loadConfig', function () {
+    beforeEach(function () {
+      this.sinon.stub(console, 'error');
+      this.sinon.spy(mean.constructor, 'getConfig');
+    });
+
+    it('should console.error notice about deprecated loadConfig', function () {
+      mean.loadConfig();
+      expect(console.error.calledOnce).to.be.true;
+    });
+
+    it('should call getConfig', function() {
+      mean.loadConfig();
+      expect(mean.constructor.getConfig.calledOnce).to.be.true;
+    });
+
+    it('should return getConfig', function() {
+      expect(mean.loadConfig() === mean.constructor.getConfig());
+    })
   });
 });
