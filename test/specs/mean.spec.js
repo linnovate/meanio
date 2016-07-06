184c0bf3a1af66fcc5ccf93331036145f27b061f
Christopher Lowenthal <clowenthal@hybridgames.net>
2016-07-06 01:16:22 -0400

    Builds basic tests

--- test/specs/mean.spec.js
+++ test/specs/mean.spec.js
@@ -5,10 +5,17 @@ var chai = require('chai'),
 
 var mean = require('../..');
 
-describe('MEAN IO Core', function() {
-  describe('Config', function() {
-    it('should load config based on NODE_ENV and NODE_CONFIG_DIR environmental vars', function() {
+describe('MEAN IO Core', function () {
+  describe('Config', function () {
+    it('should load config based on NODE_ENV and NODE_CONFIG_DIR environmental vars', function () {
       expect(mean.getConfig()._test).to.be.true;
+    });
+  });
+
+  describe('Menus', function () {
+    var Menus = require('../../lib/core_modules/menu');
+    it('should be the same as exported', function () {
+      expect(mean.Menus === Menus).to.be.true;
     });
   });
 });
