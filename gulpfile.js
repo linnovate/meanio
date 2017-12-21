c52b8c72762addac5c4f2a8c2841cceaa3a5e10f
Utkarsh shukla <utkarshthehellmaster@gmail.com>
2017-12-21 23:49:36 +0530

    Added functionality of path defining in the file.
    We can also use the other path specified, because in many cases the first case being defined doesn't work.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp'),
 
 gulp.task('test', function(){
   process.env.NODE_ENV = "test";
-  process.env.NODE_CONFIG_DIR = './test/config';
+  process.env.NODE_CONFIG_DIR = './test/config'; //sometimes some locations don't work with this syntax, hence using "../" maybe helpful.
 
   return gulp.src('./test/**/*.spec.js', {read: false})
     .pipe(mocha());
